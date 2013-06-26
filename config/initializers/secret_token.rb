1df5d6f6e82e1ddc507073fb538855528a5a3411
stevenson.jean-pierre@sage.com <stevenson.jean-pierre@sage.com>
2013-06-26 15:44:26 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DC::Application.config.secret_token = 'b6c9998a862fe664835bd6027e05f0ea910d1306126e09fda782e39a147fe8829725578eb0e7cb4f90f48bf518270eab531968308ebe037a8ba96d4627e4c18f'
